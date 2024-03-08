62cfa7518ef7e86e91ab1e0bd100c621e04a53e8
Cocomong98 <yonghjjm0618@gmail.com>
2024-03-08 23:48:25 +0900

    insertion

--- heap/insertion.cpp
+++ heap/insertion.cpp
@@ -5,10 +5,15 @@ using namespace std;
 삽입 정렬 : 두 번째 자료부터 시작 (key)
 
 8 5 6 2 4 라고 가정
+
+key의 왼쪽부터 시작해 비교함 
+
+1) key 보다 큼 : 현재 위치값을 그 값으로 바꾸고 i-1
+2) key 보다 작음 : 
 */
 
 int main(){
      
-
+    
     return 0;
 }
